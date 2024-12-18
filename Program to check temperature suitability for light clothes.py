ae08922c4adca87d314b80844f0a5ee76e0db5c8
urbaji <145362879+urbaji@users.noreply.github.com>
2024-12-18 18:38:41 +0500

    Add files via upload

---
+++ Program to check temperature suitability for light clothes.py
@@ -0,0 +1,6 @@
+temperature = int(input("Enter the current temperature (in °C): "))
+
+if temperature <= 20:
+    print("The temperature is not suitable for wearing light clothes.")
+else:
+    print("The temperature is suitable for wearing light clothes.")
